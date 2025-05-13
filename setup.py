af86cdd288771367dcea78cff88311255594c6ad
Nikita Bosov <nikita.bosov@jetbrains.com>
2025-05-13 14:46:10 +0400

    Add debug print in file

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ import re
 import subprocess
 import json
 # setup.py dependant on boto3
-subprocess.run(
+process = subprocess.run(
         ["python", "-m", "pip", "install", "boto3"],
-        check=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
+        check=False,
+        text=True,
+        stderr=subprocess.STDOUT
     )
 
+with open("/home/datalore/setup_log.txt", "w+") as f:
+    f.write(process.stdout)
