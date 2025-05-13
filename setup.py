aa59b0397f8d6720504897b74a2f476eed295042
Nikita Bosov <nikita.bosov@jetbrains.com>
2025-05-13 15:27:44 +0400

    Add which python command

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ import re
 import subprocess
 import json
 # setup.py dependant on boto3
+which_python_process = subprocess.run(
+    ["which", "python"],
+    check=False,
+    encoding="utf-8",
+    capture_output=True
+)
 process = subprocess.run(
         ["python", "-m", "pip", "install", "boto3"],
         check=False,
@@ -12,6 +18,8 @@ process = subprocess.run(
     )
 
 with open("/home/datalore/setup_log.txt", "w+") as f:
+    f.write("Which python:")
+    f.write(which_python_process.stdout)
     f.write(process.stdout)
     f.write("\nError output:\n")
     f.write(process.stderr)
